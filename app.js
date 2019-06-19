5359b61a5354be90118d6136a67b7eb2fd897a29
Pratik Lunagaria <pratiklunagaria@hotmail.com>
2019-06-19 18:23:35 +0530

    relative path

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ var hbs = exphbs.create({
 app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, './public')));
 // Routing setup
 app.use('/', indexRouter);
 app.use('/dl', dlRouter);

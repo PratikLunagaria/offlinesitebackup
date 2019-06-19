15e40a32b8351f37b92269f5d0f5ef29ad5740b3
Pratik Lunagaria <pratiklunagaria@hotmail.com>
2019-06-19 19:04:19 +0530

    fixed

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 app.use('/',express.static(path.join(__dirname, 'public')));
+app.use('/',express.static(path.join(__dirname, 'zipped')));
 // Routing setup
 app.use('/', indexRouter);
 app.use('/dl', dlRouter);
